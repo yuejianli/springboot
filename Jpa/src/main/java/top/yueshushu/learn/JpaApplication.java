58ff1f888fecd5be1e82fa85d29b1bfc369ebe43
yjl <qwe@163.com>
2021-07-24 15:53:33 +0800

    feat: 添加静态资源引用Bootstrap框架

---
+++ Jpa/src/main/java/top/yueshushu/learn/JpaApplication.java
@@ -0,0 +1,24 @@
+package top.yueshushu.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @ClassName:YamlApplication
+ * @Description jpa 讲解
+ * @Author 岳建立
+ * @Date 2021/4/26 12:01
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
+public class JpaApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(JpaApplication.class,args);
+        System.out.println("运行 jpa 配置文件");
+    }
+}
