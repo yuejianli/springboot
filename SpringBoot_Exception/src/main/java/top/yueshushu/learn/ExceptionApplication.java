c7502acb67a3904f2f9dee2cedc7ea093d8bd84c
yjl <qwe@163.com>
2021-11-12 18:07:45 +0800

    feat:springboot 的异常处理

---
+++ SpringBoot_Exception/src/main/java/top/yueshushu/learn/ExceptionApplication.java
@@ -0,0 +1,19 @@
+package top.yueshushu.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName:ExceptionApplication
+ * @Description 全局异常处理
+ * @Author zk_yjl
+ * @Date 2021/11/4 20:34
+ * @Version 1.0
+ * @Since 1.0
+ **/
+@SpringBootApplication
+public class ExceptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionApplication.class,args);
+    }
+}
