fa2ff2c845979eca5b9b616176c5a46834e669d4
yuejianli <1290513799@qq.com>
2023-04-03 17:05:13 +0800

    feat: SpringBoot 单元测试处理

---
+++ SpringBoot_Test/src/main/java/top/yueshushu/learn/TestApplication.java
@@ -0,0 +1,21 @@
+package top.yueshushu.learn;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName:YamlApplication
+ * @Description jpa 讲解
+ * @Author 岳建立
+ * @Date 2021/4/26 12:01
+ * @Version 1.0
+ **/
+@MapperScan("top.yueshushu.learn.mapper")
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+        System.out.println("运行 Test 配置文件");
+    }
+}
